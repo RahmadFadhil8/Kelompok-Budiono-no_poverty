ebc6b8c4eb5f89b14c5398024426627f9dc323f1
Dedi-Nainggolan <dedinainggolan397@gmail.com>
2025-10-25 09:08:26 +0700

    hilang

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:no_poverty/screens/auth/login.dart';
-import 'package:no_poverty/screens/auth/register.dart';
-import 'package:no_poverty/screens/home/home.dart';
 import 'package:no_poverty/app_wrapper.dart';
-import 'package:no_poverty/screens/welcome/welcome_screen.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:no_poverty/provider/chatbot_provider.dart';
+import 'package:no_poverty/screens/search/job_active.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+  create: (_) => ChatbotProvider(),
+  child: const MyApp(),
+));
+
 }
 
 class MyApp extends StatelessWidget {
@@ -44,12 +44,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: 
-      // WelcomeScreen(),
-      // LoginScreen()
-      RegisterScreen()
-
-// AppWrapper(),
-
+          AppWrapper(),
     );
   }
 }
