ca9f3941b74d88338869fe9c8181138cd738b418
Boyzest-Saragih <boyzestsumbayak@gmail.com>
2025-11-06 13:49:12 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:no_poverty/app_wrapper.dart';
 import 'package:no_poverty/provider/chatbot_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(ChangeNotifierProvider(
   create: (_) => ChatbotProvider(),
   child: const MyApp(),
